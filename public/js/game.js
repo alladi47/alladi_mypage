4ec5e7e10020f5bd569a2669e61cc7fc58a59d57
ANURAG ALLADI <S533567@nwmissouri.edu>
2018-10-29 21:41:48 -0500

    test cases

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,19 @@
 function weightConverter(valNum) {
+
+
     document.getElementById("outputPounds").innerHTML=valNum*2.2046;
+
   
     document.getElementById("outputGrams").innerHTML=valNum*1000;
+    i = valNum;
+  }
+
+  function a(i)
+  {
+    if (typeof i !== 'number') {
+      throw Error('not a number');
+    }
+   
+
+    return 0;
   }
